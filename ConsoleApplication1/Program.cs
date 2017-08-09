a1e02700feb2a48bd35a56962103808c111b2b0c
Anna Chernykh <annchernykh@gmail.com>
2017-08-10 00:29:35 +0300

    Пробная программа для работы с шаблонами ворд

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Word;
+using System.Reflection;
+
+
+
+
+
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Application appl = new Application();
+            Object templatePathObj = Environment.CurrentDirectory + "\\Shablom.dot";
+            Microsoft.Office.Interop.Word.Document doc = appl.Documents.Add(templatePathObj);
+
+            doc.Bookmarks["TYPE"].Range.Text = "ТВВ-320";
+            doc.Bookmarks["POWER"].Range.Text = "320";
+            doc.Bookmarks["VOLTAGE"].Range.Text = "20";
+            doc.Bookmarks["IMAGE"].Range.InlineShapes.AddPicture(Environment.CurrentDirectory + "\\Koala.jpg");
+            doc.SaveAs(FileName: Environment.CurrentDirectory + "\\For_print.docx");
+            appl.Quit();
+
+
+
+        }
+    }
+}
+
